2bb00947cb3cdccf0e4391c69f70ee1d705a3dec
Vijay <Vijaymaramreddy2000@gmail.com>
2023-08-08 22:05:54 +0530

    here we have given for color gradient in main class ie, color gradient is passed throught variables, so that the gradient_container.dart be reused for same functionality

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -2,18 +2,40 @@ import "package:flutter/material.dart";
 import "package:first_app/style_text.dart";
 var beginAlignment=Alignment.topLeft;
 var endAlignment=Alignment.bottomRight;
+// class GradientContainer extends StatelessWidget{
+//   const GradientContainer(this.colors1,this.colors2,{super.key});
+//   final Color colors1;
+//   final Color colors2;
+//   @override
+//   Widget build(context)
+//   {
+//     return Container(
+//         decoration:BoxDecoration(
+//           gradient: LinearGradient(
+//             colors:[ colors1,colors2],
+//             begin: beginAlignment,
+//             end: endAlignment,
+//           ),
+//         ),
+//         child:const Center(
+//           child:StyleText("hello World!, welcome to flutter"),
+//         )
+//
+//     );
+//   }}
+
+
+
 class GradientContainer extends StatelessWidget{
-  const GradientContainer({super.key});
+  const GradientContainer({super.key,required this.colors});
+  final List<Color> colors;
   @override
   Widget build(context)
   {
     return Container(
         decoration:BoxDecoration(
           gradient: LinearGradient(
-            colors:const [
-              Color.fromARGB(255, 182, 24, 113),
-              Color.fromARGB(255, 30, 78, 211),
-            ],
+            colors: colors,
             begin: beginAlignment,
             end: endAlignment,
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,8 @@ void main() {
   runApp(
       const MaterialApp(
         home:Scaffold(
-          body: GradientContainer(),
+          // body: GradientContainer(Color.fromARGB(255, 68, 61, 238), Color.fromARGB(255, 190, 130, 140)),
+          body: GradientContainer( colors: [Color.fromARGB(255, 68, 61, 238), Color.fromARGB(255, 190, 130, 140)]),
         ),
       ),
   );
