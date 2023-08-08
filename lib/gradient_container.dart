6156f8a5f50aa63c7a29e1b7b5344f6d3b85c2af
Vijay <Vijaymaramreddy2000@gmail.com>
2023-08-08 23:08:34 +0530

    to give image,first create a folder assts/images/image1.png then open pubspec.yaml and search for commented assets and do as below: assets:  - assets/image/image1.png

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -2,17 +2,40 @@ import "package:flutter/material.dart";
 import "package:first_app/style_text.dart";
 var beginAlignment=Alignment.topLeft;
 var endAlignment=Alignment.bottomRight;
+class GradientContainer extends StatelessWidget{
+  const GradientContainer(this.colors1,this.colors2,{super.key});
+  final Color colors1;
+  final Color colors2;
+  @override
+  Widget build(context)
+  {
+    return Container(
+        decoration:BoxDecoration(
+          gradient: LinearGradient(
+            colors:[ colors1,colors2],
+            begin: beginAlignment,
+            end: endAlignment,
+          ),
+        ),
+        child: Center(
+          child:Image.asset('assets/images/dice-5.png',width: 200,),
+        )
+
+    );
+  }}
+
+
+
 // class GradientContainer extends StatelessWidget{
-//   const GradientContainer(this.colors1,this.colors2,{super.key});
-//   final Color colors1;
-//   final Color colors2;
+//   const GradientContainer({super.key,required this.colors});
+//   final List<Color> colors;
 //   @override
 //   Widget build(context)
 //   {
 //     return Container(
 //         decoration:BoxDecoration(
 //           gradient: LinearGradient(
-//             colors:[ colors1,colors2],
+//             colors: colors,
 //             begin: beginAlignment,
 //             end: endAlignment,
 //           ),
@@ -23,26 +46,3 @@ var endAlignment=Alignment.bottomRight;
 //
 //     );
 //   }}
-
-
-
-class GradientContainer extends StatelessWidget{
-  const GradientContainer({super.key,required this.colors});
-  final List<Color> colors;
-  @override
-  Widget build(context)
-  {
-    return Container(
-        decoration:BoxDecoration(
-          gradient: LinearGradient(
-            colors: colors,
-            begin: beginAlignment,
-            end: endAlignment,
-          ),
-        ),
-        child:const Center(
-          child:StyleText("hello World!, welcome to flutter"),
-        )
-
-    );
-  }}
--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ void main() {
   runApp(
       const MaterialApp(
         home:Scaffold(
-          // body: GradientContainer(Color.fromARGB(255, 68, 61, 238), Color.fromARGB(255, 190, 130, 140)),
-          body: GradientContainer( colors: [Color.fromARGB(255, 68, 61, 238), Color.fromARGB(255, 190, 130, 140)]),
+          body: GradientContainer(Color.fromARGB(255, 68, 61, 238), Color.fromARGB(255, 190, 130, 140)),
+          // body: GradientContainer( colors: [Color.fromARGB(255, 68, 61, 238), Color.fromARGB(255, 190, 130, 140)]),
         ),
       ),
   );
